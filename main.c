54508854c106d65c7a2dc5fe76b0358a4d090fa6
Mitch <mitchell.stephens95@gmail.com>
2019-04-05 02:41:34 +0000

    Not much completed end of lab 1 - need to plan on paper

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 
 int main () {
 
+char c;
+//a drop down menu will be useful to selct which operation we are performing (Task 1-6) Tuesday week 6 lecture
+printf("Please select an option: \n");
+printf("1) Encrypt message with Cesar cypher\n");
+printf("2) Decrypyt message with Cesar cypher\n");
+printf("3) Encrypt message using substitution cypher\n");
+printf("4) Decrypt message using substitution cypher\n");
+printf("Option number: ",);
+// these 4 options need to be completed fot a pass mark
+scanf("%c", &c);
+//once we select the option then we can link it to which section of the code we are executing and link that to the appropraite functions
+// using switch/case??
+
+   
   return 0;
+
 }
